fa106c3e01cb28d52215fe9b6f2d66e1675bb35a
swkashita <rebeka.sakashita@outlook.com>
2026-05-20 08:08:55 -0300

    exercicios abrindo porem n bonito

--- src/main/java/com/ifsp/Auroraprojeto/ConteudoRepository.java
+++ src/main/java/com/ifsp/Auroraprojeto/ConteudoRepository.java
@@ -1,16 +1,18 @@
 package com.ifsp.Auroraprojeto;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ConteudoRepository extends JpaRepository<Conteudo, Long> {
-
+    
+    // Busca por tipo (usado nos materiais extras e provas)
     List<Conteudo> findByTipo(TipoConteudo tipo);
-
-    List<Conteudo> findByDisciplinaAndNivel(
-            Disciplina disciplina,
-            String nivel
-    );
-
+    
+    // Busca por disciplina e nível (usado nas aulas)
+    List<Conteudo> findByDisciplinaAndNivel(Disciplina disciplina, String nivel);
+    
+    // ADICIONE ESTA LINHA AQUI PARA CORRIGIR O ERRO 500:
+    List<Conteudo> findByNivel(String nivel);
 }
