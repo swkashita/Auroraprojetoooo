06d7ed5712ddc13a26e24a55d3ee94350614ff7d
swkashita <rebeka.sakashita@outlook.com>
2026-05-20 08:57:59 -0300

    tela exercicios FUNCIONANDO jnto com o ADMIN

--- src/main/java/com/ifsp/Auroraprojeto/TipoConteudo.java
+++ src/main/java/com/ifsp/Auroraprojeto/TipoConteudo.java
@@ -1,8 +1,9 @@
 package com.ifsp.Auroraprojeto;
 
 public enum TipoConteudo {
-    PROVA,
     VIDEO,
-    EXERCICIO,
-    MATERIAL // <-- ADICIONE ESSA LINHA AQUI
+    PROVA,
+    GABARITO,
+    MATERIAL,
+    EXERCICIO  // Adicione este aqui
 }
